ec7db625d4c92532482cc3709cc2083682464dec
Stefan Pataky <stefan.pataky@gmail.com>
2013-10-01 12:15:46 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+express = require('express');
+
+
+var ihex = require('intel-hex');
+var fs = require('fs');
+
+var app = express();
+
+app.configure(function() {
+  app.use('/', express.static('./static'));
+});
+
+
+app.get('/avr', function(req,res){
+  fs.readFile('blinkp.hex', function(err, file) {
+    var hex = ihex.parse(file);
+
+    res.send(hex);
+  });
+});
+
+app.listen(3000);
